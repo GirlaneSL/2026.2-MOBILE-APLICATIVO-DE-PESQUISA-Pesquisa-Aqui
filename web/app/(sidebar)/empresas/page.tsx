77c6ec1b8f00372377288c912bd8bc7a794d340d
kaiky nonato <kaikynonato288br@gmail.com>
2026-09-03 00:04:19 -0300

    build: react hot toast

--- web/app/(sidebar)/empresas/page.tsx
+++ web/app/(sidebar)/empresas/page.tsx
@@ -9,7 +9,7 @@ export default function EmpresaPage() {
     return (
         <>
             <section className="flex flex-col gap-6">
-                <BannerComponent title="Empresas"></BannerComponent>
+                <BannerComponent title="Dados das Empresas"></BannerComponent>
                 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     <InfoCard
                         isCardFooter={false}
